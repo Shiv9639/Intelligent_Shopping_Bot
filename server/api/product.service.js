83fe94ca8278f3cfd625bb56a65a2633a84af509
neel2468 <pneel813@gmail.com>
2020-11-25 18:38:23 +0530

    completed bot

--- server/api/product.service.js
+++ server/api/product.service.js
@@ -4,35 +4,31 @@ module.exports={
 
     
     getProducts:(price,category,brand,color,gender,type,capacity,description,callBack) =>{
-
-        
-
-
         let query="select * from Product where 1=1"
        
         if(category!==undefined){
-            query=query+ " or LOWER(product_category) like '%"+category+"%'";
+            query=query+ " and LOWER(product_category) like '%"+category+"%'";
         }
         if(price!==undefined){
-            query=query+ " or product_price <= "+parseFloat(price);
+            query=query+ " and product_price <= "+parseFloat(price);
         }
         if(brand!==undefined){
-            query=query+ " or LOWER(brand) like '%"+brand+"%'";
+            query=query+ " and LOWER(product_brand) like '%"+brand+"%'";
         }
         if(color!==undefined){
-            query=query+ " or LOWER(product_color) like '%"+color+"%'";
+            query=query+ " and LOWER(product_color) like '%"+color+"%'";
         }
         if(gender!==undefined){
-            query=query+ " or LOWER(gender) like '%"+gender+"%'";
+            query=query+ " and LOWER(gender) like '%"+gender+"%'";
         }
         if(type!==undefined){
-            query=query+ " or LOWER(product_types) like '%"+type+"%'";
+            query=query+ " and LOWER(product_types) like '%"+type+"%'";
         }
         if(capacity!==undefined){
-            query=query+ " or LOWER(capacity) like '%"+capacity+"%'";
+            query=query+ " and LOWER(capacity) like '%"+capacity+"%'";
         }
         if(description!==undefined){
-            query=query+ " or LOWER(product_description) like '%"+description+"%'";
+            query=query+ " and LOWER(product_description) like '%"+description+"%'";
         }
       
        
